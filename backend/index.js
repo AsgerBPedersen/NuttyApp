2b6c4665b03be7e51edf126b77879d5783004e64
unknown <inzolin2@gmail.com>
2019-06-04 16:53:04 +0200

    graphql work

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+
 const app = express();
+
+const foods = [];
 
 app.use(bodyParser.json());
 
@@ -10,12 +13,29 @@ app.use(
     '/graphql',
      graphqlHttp({
     schema: buildSchema(`
+        type Food {
+            _id: ID!
+            name: String!
+            kcal: Float!
+            protein: Float!
+            fat: Float!
+            carbs: Float!
+        }
+
+        input FoodInput {
+            name: String!
+            kcal: Float!
+            protein: Float!
+            fat: Float!
+            carbs: Float!
+        }
+
         type RootQuery {
-            foods: [String!]!
+            foods: [Food!]!
         }
 
         type RootMutation {
-            createFood(name: String): String
+            createFood(foodInput: FoodInput): Food
         }
         schema {
             query: RootQuery
@@ -24,11 +44,19 @@ app.use(
     `),
     rootValue: {
         foods: () => {
-            return ['apple', 'ham', 'cheese'];
+            return foods;
         },
         createFood: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const food = {
+                _id: Math.random().toString(),
+                name: args.foodInput.name,
+                kcal: +args.foodInput.kcal,
+                protein: +args.foodInput.protein,
+                fat: +args.foodInput.fat,
+                carbs: +args.foodInput.carbs
+            };
+            foods.push(food);
+            return food;
         }
     },
     graphiql: true
