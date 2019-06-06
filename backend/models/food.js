56d9233278ba151ecd8bc471517064f3035c1258
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2019-06-06 14:04:35 +0200

    added user reference to food model

--- backend/models/food.js
+++ backend/models/food.js
@@ -26,6 +26,10 @@ const foodSchema = new Schema({
     date: {
         type: Date,
         required: true
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     }
 });
 
